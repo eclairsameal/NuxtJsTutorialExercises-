dc601d2696f033055adf595f6dc0668449a50c13
eclairsameal <skolleclair@gmail.com>
2023-06-28 15:37:47 +0900

    Dynamically generate paths based on input

--- cartrader/components/Car/SearchBar.vue
+++ cartrader/components/Car/SearchBar.vue
@@ -1,11 +1,20 @@
+<script setup>
+    const city = ref("");
+    //const router = useRouter();
+    const handleSearch = () => {
+        //router.push(`/city/${city.value}/car`)
+        navigateTo(`/city/${city.value}/car`);
+    }
+</script>
 <template>
     <div
         class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
         <input
-        type="text"
-        class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
-        placeholder="Search by city..."
+            type="text"
+            class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+            placeholder="Search by city..."
+            v-model="city"
         />
-        <button class="bg-sky-500 px-10 text-white">Search</button>
+        <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
     </div>
 </template>
