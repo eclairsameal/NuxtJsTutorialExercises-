ec524889eff560da5e6cc0c37f5042a7743a33c2
eclairsameal <skolleclair@gmail.com>
2023-06-28 14:36:16 +0900

    Decompose the cars.html page into Card Cards SideBar, and call it in [[make.vue]].

--- cartrader/pages/city/[city]/car/[[make]].vue
+++ cartrader/pages/city/[city]/car/[[make]].vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        <h1>city and make</h1>
+        <NavBar/>
+        <!-- CARS PAGE -->
+        <div
+            class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+        >
+        <div class="mt-32 flex">
+        <!-- CAR SIDE BAR -->
+            <CarSideBar/>
+        <!-- CAR SIDE BAR -->
+
+        <!-- CAR CARDS -->
+        <!-- CAR CARD -->
+            <CarCards/>
+        <!-- CAR CARD -->
+        <!-- CAR CARDS -->
+       </div>
+      </div>
+    <!-- CARS PAGE -->
     </div>
 </template>
